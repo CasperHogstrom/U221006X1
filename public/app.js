015e3dfa2676a532f4bd8bbb0911ab83981655a4
CasperHogstrom <casperhogstrom3@gmail.com>
2022-10-06 12:54:16 +0200

    Tried the post method, it worked

--- public/app.js
+++ public/app.js
@@ -1,8 +1,12 @@
 var express = require('express');
 var app = express();
 
-app.get('/', function(req, res){
+app.get('/hello', function(req, res){
    res.send("Hello world!");
+});
+
+app.post('/hello', function(req, res){
+    res.send("You just called the post(man) method at '/hello'!\n");
 });
 
 app.listen(3000);
