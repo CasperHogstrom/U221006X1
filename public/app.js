9f9b03696ddf96a474bdb07078f40abaf7e14dbe
CasperHogstrom <casperhogstrom3@gmail.com>
2022-10-06 13:27:21 +0200

    Now has a picture and an H1, had a problem that was solved by launching nodemon differently

--- public/app.js
+++ public/app.js
@@ -1,12 +1,7 @@
 var express = require('express');
 var app = express();
 
-app.get('/hello', function(req, res){
-   res.send("Hello world!");
-});
+app.use(express.static('public'));
 
-app.post('/hello', function(req, res){
-    res.send("You just called the post(man) method at '/hello'!\n");
-});
 
 app.listen(3000);
